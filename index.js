ec99fd029ea8d837505e11949360421693deec28
mgDentist <gmi1987@gmail.com>
2023-08-21 15:55:04 +0400

    module1 part19 hometask2

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hello World!');
+import "./index.css";
+import MY_IMAGE from './assets/js-icon.png';
+
+const createHtml= () => {
+    const h1 = document.createElement('h1');
+    h1.className = 'main__title'
+    h1.textContent = 'I love JavaScript';
+
+    const img = document.createElement('img');
+    img.className = 'main__img';
+    img.setAttribute('width', '400');
+    img.setAttribute('height', '400');
+    img.src = MY_IMAGE;
+    img.alt = 'JS logo';
+
+    document.body.append(h1);
+    document.body.append(img);
+};
+
+createHtml();
+
+
--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,9 @@
+import { babel } from '@rollup/plugin-babel';
+import styles from 'rollup-plugin-styles';
+import image from '@rollup/plugin-image';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
 export default {
     input: './index.js',
     output: {
@@ -6,5 +12,15 @@ export default {
     },
     watch: {
         exclude: 'node_modules/**'
-    }
+    },
+    plugins: [
+        babel({
+            babelHelpers: "bundled",
+            presets: ["@babel/env"]
+        }),
+        styles(),
+        image(),
+        serve({ open: true }),
+        livereload()
+    ]
 };
